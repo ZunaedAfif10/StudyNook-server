ed9bc11fa8627514bf7e38380327130851b35d0a
ZunaedAfif10 <zunaedrahman7@gmail.com>
2026-05-21 14:56:18 +0600

    added mybooking get

--- index.js
+++ index.js
@@ -51,21 +51,31 @@ async function run() {
 
       res.json(result);
     });
-
+    
+    
     app.post("/rooms", async (req, res) => {
       const roomsdata = req.body;
       // console.log(roomsdata);
       const result = await roomsCollection.insertOne(roomsdata);
+      
+      res.json(result);
+    });
+    
+    app.get("/bookings/:userId", async (req, res) => {
+    const { userId } = req.params;
+
+      const result = await bookingCollection.find({ user_Id: userId }).toArray();
 
       res.json(result);
     });
-
+    
     app.post('/bookings', async (req, res) => {
       const bookingData = req.body;
-      const { date, startTime, endTime } = bookingData
+      const {roomName, date, startTime, endTime } = bookingData
       // console.log(date,startTime,endTime)
       
       const conflict = await bookingCollection.findOne({
+        roomName,
         date,
         $and: [
           {
